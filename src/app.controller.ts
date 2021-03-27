c58f45113ace8d7e8512f72433396c295b950b13
Chung Tran <pirate1912@gmail.com>
2021-03-28 07:28:03 +0900

    return 404 at /

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @HttpCode(200)
-  getHello() {
-    return this.appService.getHello();
-  }
+  // @Get()
+  // @HttpCode(200)
+  // getHello() {
+  //   return this.appService.getHello();
+  // }
 }
